f106c44e67c9e021ca5314ca35d3bfb38436ae91
Aliaboro <aliya.borovik@gmail.com>
2024-10-09 00:36:57 +0300

    created new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [places, setPlaces] = useState(data);
+    const [showText, setShowText] = useState(false);
+    const removePlace = (id) => {
+      let newPlaces = places.filter((place) => place.id !== id);
+      setPlaces(newPlaces)
+    }
+    const showTextClick = (item) => {
+       item.showMore = !item.showMore
+       setShowText(!showText)
+    }
+  
+    return(<div>
+      <div className="container">
+        <h1> Топ {places.length} мест для посещения в Ереване</h1>
+      </div>
+     {places.map((item => {
+      const {id, placeName, description, image, showMore } = item;
+      return(
+      <div key={id}>
+        <div className="container">
+          <h2>{id} - {placeName} </h2>
+          <p> {showMore ? description : description.substring(0,190) + "..."}
+          <button onClick ={() => showTextClick(item)}>{showMore ? "Свернуть текст" : "Развернуть текст"}</button></p>
+          <img src={image} alt="Place in Erevan" width="300px"/>
+        </div>
+  
+        <div className="container">
+          <button className="btn" onClick={() => removePlace(id)}>Удалить из списка</button>
+        </div>
+  
+      </div>
+      )
+    }))}
+    <div className="container">
+    <button className="btn2" onClick={() => setPlaces([])}>Удалить все</button>
+    </div>
+    </div>
+    )
+  }
+  
+  export default App;
+  
